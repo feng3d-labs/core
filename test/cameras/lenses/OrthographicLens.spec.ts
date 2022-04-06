6648fde431a9ea3f7c52987daa7913759557f442
feng <908087098@qq.com>
2022-04-06 11:05:36 +0800

    OrthographicLens.spec

--- test/cameras/lenses/OrthographicLens.spec.ts
+++ test/cameras/lenses/OrthographicLens.spec.ts
@@ -3,136 +3,135 @@
 // import { ok } from 'assert';
 // import { OrthographicLens } from '../../../src';
 
-// var NUM = 10;
+// const NUM = 10;
 
-// describe("OrthographicLens", () =>
+// describe('OrthographicLens', () =>
 // {
-//     it("project", () =>
+//     it('project', () =>
 //     {
 //         // 生成随机正交矩阵
-//         var size = Math.random();
-//         var near = Math.random();
-//         var far = Math.random() + near;
+//         const size = Math.random();
+//         const near = Math.random();
+//         const far = Math.random() + near;
 
-//         var orthographicLens = new OrthographicLens(size, 1, near, far);
+//         const orthographicLens = new OrthographicLens(size, 1, near, far);
 
 //         // 测试可视空间的8个顶点是否被正确投影
-//         var lbn = new Vector3(-size, -size, near);
-//         var tv = orthographicLens.project(lbn);
+//         const lbn = new Vector3(-size, -size, near);
+//         let tv = orthographicLens.project(lbn);
 //         ok(new Vector3(-1, -1, -1).equals(tv));
 
-//         var lbf = new Vector3(-size, -size, far);
-//         var tv = orthographicLens.project(lbf);
+//         const lbf = new Vector3(-size, -size, far);
+//         tv = orthographicLens.project(lbf);
 //         ok(new Vector3(-1, -1, 1).equals(tv));
 
-//         var ltn = new Vector3(-size, size, near);
-//         var tv = orthographicLens.project(ltn);
+//         const ltn = new Vector3(-size, size, near);
+//         tv = orthographicLens.project(ltn);
 //         ok(new Vector3(-1, 1, -1).equals(tv));
 
-//         var ltf = new Vector3(-size, size, far)
-//         var tv = orthographicLens.project(ltf);
+//         const ltf = new Vector3(-size, size, far);
+//         tv = orthographicLens.project(ltf);
 //         ok(new Vector3(-1, 1, 1).equals(tv));
 
-//         var rbn = new Vector3(size, -size, near);
-//         var tv = orthographicLens.project(rbn);
+//         const rbn = new Vector3(size, -size, near);
+//         tv = orthographicLens.project(rbn);
 //         ok(new Vector3(1, -1, -1).equals(tv));
 
-//         var rbf = new Vector3(size, -size, far)
-//         var tv = orthographicLens.project(rbf);
+//         const rbf = new Vector3(size, -size, far);
+//         tv = orthographicLens.project(rbf);
 //         ok(new Vector3(1, -1, 1).equals(tv));
 
-//         var rtn = new Vector3(size, size, near);
-//         var tv = orthographicLens.project(rtn);
+//         const rtn = new Vector3(size, size, near);
+//         tv = orthographicLens.project(rtn);
 //         ok(new Vector3(1, 1, -1).equals(tv));
 
-//         var rtf = new Vector3(size, size, far);
-//         var tv = orthographicLens.project(rtf);
+//         const rtf = new Vector3(size, size, far);
+//         tv = orthographicLens.project(rtf);
 //         ok(new Vector3(1, 1, 1).equals(tv));
 //     });
 
-//     it("unproject", () =>
+//     it('unproject', () =>
 //     {
 //         // 生成随机正交矩阵
-//         var size = Math.random();
-//         var near = Math.random();
-//         var far = Math.random() + near;
+//         const size = Math.random();
+//         const near = Math.random();
+//         const far = Math.random() + near;
 
-//         var orthographicLens = new OrthographicLens(size, 1, near, far);
+//         const orthographicLens = new OrthographicLens(size, 1, near, far);
 
 //         // 测试可视空间的8个顶点是否被正确投影
-//         var lbn = new Vector3(-1, -1, -1);
-//         var tv = orthographicLens.unproject(lbn);
+//         const lbn = new Vector3(-1, -1, -1);
+//         let tv = orthographicLens.unproject(lbn);
 //         ok(new Vector3(-size, -size, near).equals(tv));
 
-//         var lbf = new Vector3(-1, -1, 1);
-//         var tv = orthographicLens.unproject(lbf);
+//         const lbf = new Vector3(-1, -1, 1);
+//         tv = orthographicLens.unproject(lbf);
 //         ok(new Vector3(-size, -size, far).equals(tv));
 
-//         var ltn = new Vector3(-1, 1, -1);
-//         var tv = orthographicLens.unproject(ltn);
+//         const ltn = new Vector3(-1, 1, -1);
+//         tv = orthographicLens.unproject(ltn);
 //         ok(new Vector3(-size, size, near).equals(tv));
 
-//         var ltf = new Vector3(-1, 1, 1)
-//         var tv = orthographicLens.unproject(ltf);
+//         const ltf = new Vector3(-1, 1, 1);
+//         tv = orthographicLens.unproject(ltf);
 //         ok(new Vector3(-size, size, far).equals(tv));
 
-//         var rbn = new Vector3(1, -1, -1);
-//         var tv = orthographicLens.unproject(rbn);
+//         const rbn = new Vector3(1, -1, -1);
+//         tv = orthographicLens.unproject(rbn);
 //         ok(new Vector3(size, -size, near).equals(tv));
 
-//         var rbf = new Vector3(1, -1, 1)
-//         var tv = orthographicLens.unproject(rbf);
+//         const rbf = new Vector3(1, -1, 1);
+//         tv = orthographicLens.unproject(rbf);
 //         ok(new Vector3(size, -size, far).equals(tv));
 
-//         var rtn = new Vector3(1, 1, -1);
-//         var tv = orthographicLens.unproject(rtn);
+//         const rtn = new Vector3(1, 1, -1);
+//         tv = orthographicLens.unproject(rtn);
 //         ok(new Vector3(size, size, near).equals(tv));
 
-//         var rtf = new Vector3(1, 1, 1);
-//         var tv = orthographicLens.unproject(rtf);
+//         const rtf = new Vector3(1, 1, 1);
+//         tv = orthographicLens.unproject(rtf);
 //         ok(new Vector3(size, size, far).equals(tv));
-
 //     });
 
-//     it("unprojectRay", () =>
+//     it('unprojectRay', () =>
 //     {
-//         var size = Math.random();
-//         var near = Math.random();
-//         var far = Math.random() + near;
+//         const size = Math.random();
+//         const near = Math.random();
+//         const far = Math.random() + near;
 
-//         var orthographicLens = new OrthographicLens(size, 1, near, far);
+//         const orthographicLens = new OrthographicLens(size, 1, near, far);
 
-//         var x = Math.random();
-//         var y = Math.random();
+//         const x = Math.random();
+//         const y = Math.random();
 
 //         for (let i = 0; i < NUM; i++)
 //         {
-//             var ray = orthographicLens.unprojectRay(x, y);
-//             var p = ray.getPointWithZ(mathUtil.lerp(near, far, Math.random()));
-//             var pp = orthographicLens.project(p);
+//             const ray = orthographicLens.unprojectRay(x, y);
+//             const p = ray.getPointWithZ(mathUtil.lerp(near, far, Math.random()));
+//             const pp = orthographicLens.project(p);
 //             ok(mathUtil.equals(x, pp.x));
 //             ok(mathUtil.equals(y, pp.y));
 //         }
 //     });
 
-//     it("unprojectWithDepth", () =>
+//     it('unprojectWithDepth', () =>
 //     {
-//         var size = Math.random();
-//         var near = Math.random();
-//         var far = Math.random() + near;
+//         const size = Math.random();
+//         const near = Math.random();
+//         const far = Math.random() + near;
 
-//         var orthographicLens = new OrthographicLens(size, 1, near, far);
+//         const orthographicLens = new OrthographicLens(size, 1, near, far);
 
-//         var x = Math.random();
-//         var y = Math.random();
+//         const x = Math.random();
+//         const y = Math.random();
 
 //         for (let i = 0; i < NUM; i++)
 //         {
-//             var sZ = mathUtil.lerp(near, far, Math.random());
-//             var p = orthographicLens.unprojectWithDepth(x, y, sZ);
+//             const sZ = mathUtil.lerp(near, far, Math.random());
+//             const p = orthographicLens.unprojectWithDepth(x, y, sZ);
 //             ok(mathUtil.equals(sZ, p.z));
 
-//             var pp = orthographicLens.project(p);
+//             const pp = orthographicLens.project(p);
 //             ok(mathUtil.equals(x, pp.x));
 //             ok(mathUtil.equals(y, pp.y));
 //         }
