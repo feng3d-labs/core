aa9876c5091d35391b1de829f854fa3722b1de64
feng <908087098@qq.com>
2022-07-18 17:53:44 +0800

    MixinsGlobalEvents

--- src/MixinsGlobalEvents.ts
+++ src/MixinsGlobalEvents.ts
@@ -11,5 +11,21 @@ namespace feng3d
          * 脚本发生变化
          */
         'asset.scriptChanged': any;
+        /**
+         * 图片资源发生变化
+         */
+        'asset.imageAssetChanged': { url: string };
+        /**
+         * 解析出资源
+         */
+        'asset.parsed': any;
+        /**
+         * 删除文件
+         */
+        'fs.delete': string;
+        /**
+         * 写文件
+         */
+        'fs.write': string;
     }
 }
